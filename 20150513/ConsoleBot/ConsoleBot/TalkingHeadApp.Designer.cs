d50813aee3a76a69872142225375696e9cdb7a99
saincogt <saincogt@gmail.com>
2015-05-13 15:53:50 +0800

    20150513
    Code for 20150513

---
+++ 20150513/ConsoleBot/ConsoleBot/TalkingHeadApp.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace ConsoleBot
+{
+    partial class TalkingHeadApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TalkingHeadApp));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newChatToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutAIMLToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutAtlasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutTheTeamToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.talkToAtlasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.talkToAliceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.configuresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 491);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(635, 37);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(653, 491);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 37);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "ENTER";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(232, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(496, 419);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox2.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox2.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textBox2.Location = new System.Drawing.Point(12, 30);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(214, 454);
+            this.textBox2.TabIndex = 4;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.configuresToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(740, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newChatToolStripMenuItem,
+            this.talkToAtlasToolStripMenuItem,
+            this.talkToAliceToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(74, 20);
+            this.fileToolStripMenuItem.Text = "I want to...";
+            // 
+            // newChatToolStripMenuItem
+            // 
+            this.newChatToolStripMenuItem.Name = "newChatToolStripMenuItem";
+            this.newChatToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.newChatToolStripMenuItem.Text = "Start a new chat";
+            this.newChatToolStripMenuItem.Click += new System.EventHandler(this.newChatToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutAIMLToolStripMenuItem,
+            this.aboutAtlasToolStripMenuItem,
+            this.aboutTheTeamToolStripMenuItem});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // aboutAIMLToolStripMenuItem
+            // 
+            this.aboutAIMLToolStripMenuItem.Name = "aboutAIMLToolStripMenuItem";
+            this.aboutAIMLToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.aboutAIMLToolStripMenuItem.Text = "About AIML";
+            this.aboutAIMLToolStripMenuItem.Click += new System.EventHandler(this.aboutAIMLToolStripMenuItem_Click);
+            // 
+            // aboutAtlasToolStripMenuItem
+            // 
+            this.aboutAtlasToolStripMenuItem.Name = "aboutAtlasToolStripMenuItem";
+            this.aboutAtlasToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.aboutAtlasToolStripMenuItem.Text = "About Atlas";
+            this.aboutAtlasToolStripMenuItem.Click += new System.EventHandler(this.aboutAtlasToolStripMenuItem_Click);
+            // 
+            // aboutTheTeamToolStripMenuItem
+            // 
+            this.aboutTheTeamToolStripMenuItem.Name = "aboutTheTeamToolStripMenuItem";
+            this.aboutTheTeamToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.aboutTheTeamToolStripMenuItem.Text = "About the Team";
+            this.aboutTheTeamToolStripMenuItem.Click += new System.EventHandler(this.aboutTheTeamToolStripMenuItem_Click);
+            // 
+            // talkToAtlasToolStripMenuItem
+            // 
+            this.talkToAtlasToolStripMenuItem.Name = "talkToAtlasToolStripMenuItem";
+            this.talkToAtlasToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.talkToAtlasToolStripMenuItem.Text = "Talk to Atlas";
+            // 
+            // talkToAliceToolStripMenuItem
+            // 
+            this.talkToAliceToolStripMenuItem.Name = "talkToAliceToolStripMenuItem";
+            this.talkToAliceToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.talkToAliceToolStripMenuItem.Text = "Talk to Alice";
+            // 
+            // configuresToolStripMenuItem
+            // 
+            this.configuresToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.optionsToolStripMenuItem});
+            this.configuresToolStripMenuItem.Name = "configuresToolStripMenuItem";
+            this.configuresToolStripMenuItem.Size = new System.Drawing.Size(77, 20);
+            this.configuresToolStripMenuItem.Text = "Configures";
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.optionsToolStripMenuItem.Text = "Options";
+            this.optionsToolStripMenuItem.Click += new System.EventHandler(this.optionsToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(740, 541);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Talking Head Application - Team Atlas";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Enter += new System.EventHandler(this.button1_Click);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newChatToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutAIMLToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutAtlasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutTheTeamToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem talkToAtlasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem talkToAliceToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem configuresToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+    }
+}
